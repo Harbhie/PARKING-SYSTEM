a60a62dc379bc01b748b7c9c62d46580445af0a5
pawiieeee <pauleenmalit65@gmail.com>
2025-12-07 00:53:47 +0800

    Update Merge_Sensor_Testing.ino

--- Merge_Sensor_Testing/Merge_Sensor_Testing.ino
+++ Merge_Sensor_Testing/Merge_Sensor_Testing.ino
@@ -1,161 +1,3 @@
-// #include <Servo.h>
-// #include <SPI.h>
-// #include <MFRC522.h>
-
-// Servo myServo;
-
-// const int irPin1 = 9;    
-// const int irPin2 = 6;     
-// const int servoPin = 8;
-
-// #define SS_PIN 10
-// #define RST_PIN 7
-
-// MFRC522 rfid(SS_PIN, RST_PIN);
-
-// byte allowedTag[4] = {0xF2, 0xCB, 0x12, 0x01};
-
-// bool hasTriggeredForward = false;
-// bool hasTriggeredBackward = false;
-
-// int count = 0;
-
-// void setup() {
-//   pinMode(irPin1, INPUT);
-//   pinMode(irPin2, INPUT);
-
-//   SPI.begin();
-//   rfid.PCD_Init();
-
-//   myServo.attach(servoPin);
-//   myServo.detach();  
-// }
-
-// bool checkRFID() {
-//   if (!rfid.PICC_IsNewCardPresent()) return false;
-//   if (!rfid.PICC_ReadCardSerial()) return false;
-
-//   for (byte i = 0; i < 4; i++) {
-//     if (rfid.uid.uidByte[i] != allowedTag[i]) return false;
-//   }
-//   return true;
-// }
-
-// void loop() {
-//   bool ir1Detected = (digitalRead(irPin1) == LOW);
-//   bool ir2Detected = (digitalRead(irPin2) == LOW);
-//   bool rfidDetected = checkRFID();
-
-//   if (count == 0 && ir1Detected && rfidDetected && !hasTriggeredForward) {
-//     hasTriggeredForward = true;
-//     count = 1;  
-
-//     myServo.attach(servoPin);
-//     myServo.write(0); 
-//     delay(400);
-//     myServo.detach();
-//   }
-
-//   if (!ir1Detected && !rfidDetected) {
-//     hasTriggeredForward = false;
-//   }
-
-//   if (count == 1 && ir2Detected && !hasTriggeredBackward) {
-//     hasTriggeredBackward = true;
-//     count = 0;  
-
-//     myServo.attach(servoPin);
-//     myServo.write(180); 
-//     delay(400);
-//     myServo.detach();
-//   }
-
-//   if (!ir2Detected) {
-//     hasTriggeredBackward = false;
-//   }
-// }
-// #include <Servo.h>
-// #include <SPI.h>
-// #include <MFRC522.h>
-
-// Servo myServo;
-
-// const int irPin1 = 3;
-// const int irPin2 = 4;
-// const int servoPin = 8;
-
-// #define SS_PIN 10
-// #define RST_PIN 7
-
-// MFRC522 rfid(SS_PIN, RST_PIN);
-
-// byte allowedTag[4] = {0xF2, 0xCB, 0x12, 0x01};
-
-// bool hasTriggeredForward = false;
-// bool hasTriggeredBackward = false;
-
-// int count = 0;
-
-// void setup() {
-//   Serial.begin(9600);
-
-//   pinMode(irPin1, INPUT);
-//   pinMode(irPin2, INPUT);
-
-//   SPI.begin();
-//   rfid.PCD_Init();
-
-//   myServo.attach(servoPin);
-//   myServo.detach();
-// }
-
-// bool checkRFID() {
-//   if (!rfid.PICC_IsNewCardPresent()) return false;
-//   if (!rfid.PICC_ReadCardSerial()) return false;
-
-//   for (byte i = 0; i < 4; i++) {
-//     if (rfid.uid.uidByte[i] != allowedTag[i]) return false;
-//   }
-//   return true;
-// }
-
-// void loop() {
-//   bool ir1Detected = (digitalRead(irPin1) == LOW);
-//   bool ir2Detected = (digitalRead(irPin2) == LOW);
-//   bool rfidDetected = checkRFID();
-
-//   if (count == 0 && ir1Detected && rfidDetected && !hasTriggeredForward) {
-//     hasTriggeredForward = true;
-//     count = 1;
-
-//     Serial.println("Servo Rotating FORWARD");
-
-//     myServo.attach(servoPin);
-//     myServo.write(0);
-//     delay(400);
-//     myServo.detach();
-//   }
-
-//   if (!ir1Detected && !rfidDetected) {
-//     hasTriggeredForward = false;
-//   }
-
-//   if (count == 1 && ir2Detected && !hasTriggeredBackward) {
-//     hasTriggeredBackward = true;
-//     count = 0;
-
-//     Serial.println("Servo Rotating BACKWARD");
-
-//     myServo.attach(servoPin);
-//     myServo.write(180);
-//     delay(400);
-//     myServo.detach();
-//   }
-
-//   if (!ir2Detected) {
-//     hasTriggeredBackward = false;
-//   }
-// }
 
 // -------------------------------- ESP32 ---------------------------------------- //
 #include <ESP32Servo.h>
@@ -164,27 +6,25 @@
 
 Servo myServo;
 
-// IR Sensor Pins
-const int irPin1 = 18;
-const int irPin2 = 21;
+// Updated pins for ESP32
+const int irPin1 = 15;     // IR1
+const int irPin2 = 14;     // IR2
+const int servoPin = 13;   // Servo (SAFE PIN)
 
-// Servo Pin (ESP32 PWM)
-const int servoPin = 5;
+// RFID Pins for ESP32
+#define SS_PIN 5
+#define RST_PIN 22
 
-// RFID Pins (ESP32 SPI)
-#define SS_PIN 22
-#define RST_PIN 3
-#define SCK_PIN 19
-#define MOSI_PIN 23
-#define MISO_PIN 25
-
+// SPI pins for ESP32: SCK=18, MISO=19, MOSI=23
 MFRC522 rfid(SS_PIN, RST_PIN);
 
-// Allowed Tag
-byte allowedTag[4] = {0xF2, 0xCB, 0x12, 0x01};
+// Allowed RFID tags
+byte allowedTag1[4] = {0xF2, 0xCB, 0x12, 0x01};
+byte allowedTag2[4] = {0x22, 0x73, 0xB4, 0xAB};
 
 bool hasTriggeredForward = false;
 bool hasTriggeredBackward = false;
+
 int count = 0;
 
 void setup() {
@@ -193,28 +33,29 @@ void setup() {
   pinMode(irPin1, INPUT);
   pinMode(irPin2, INPUT);
 
-  // ESP32 SPI initialization
-  SPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN, SS_PIN);
-
+  SPI.begin(18, 19, 23);   
   rfid.PCD_Init();
-
-  // ATTENTION: Remove invalid Servo::attach() line
-  // Correct code is below:
 
   myServo.attach(servoPin);
   myServo.detach();
 
-  Serial.println("ESP32 RFID + IR + Servo ready");
+  Serial.println("System Ready...");
 }
 
+// Check if scanned tag matches allowedTag1 OR allowedTag2
 bool checkRFID() {
   if (!rfid.PICC_IsNewCardPresent()) return false;
   if (!rfid.PICC_ReadCardSerial()) return false;
 
+  bool match1 = true;
+  bool match2 = true;
+
   for (byte i = 0; i < 4; i++) {
-    if (rfid.uid.uidByte[i] != allowedTag[i]) return false;
+    if (rfid.uid.uidByte[i] != allowedTag1[i]) match1 = false;
+    if (rfid.uid.uidByte[i] != allowedTag2[i]) match2 = false;
   }
-  return true;
+
+  return (match1 || match2);
 }
 
 void loop() {
@@ -222,34 +63,34 @@ void loop() {
   bool ir2Detected = (digitalRead(irPin2) == LOW);
   bool rfidDetected = checkRFID();
 
-  // Forward rotation
+  // Forward Trigger
   if (count == 0 && ir1Detected && rfidDetected && !hasTriggeredForward) {
     hasTriggeredForward = true;
     count = 1;
-
-    Serial.println("Servo Rotating FORWARD");
 
     myServo.attach(servoPin);
     myServo.write(0);
     delay(400);
     myServo.detach();
+
+    Serial.println("Forward Triggered");
   }
 
   if (!ir1Detected && !rfidDetected) {
     hasTriggeredForward = false;
   }
 
-  // Backward rotation
+  // Backward Trigger
   if (count == 1 && ir2Detected && !hasTriggeredBackward) {
     hasTriggeredBackward = true;
     count = 0;
-
-    Serial.println("Servo Rotating BACKWARD");
 
     myServo.attach(servoPin);
     myServo.write(180);
     delay(400);
     myServo.detach();
+
+    Serial.println("Backward Triggered");
   }
 
   if (!ir2Detected) {
